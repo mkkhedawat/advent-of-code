12eddcf7c460d72dd63687a39cdd54a933fef5e4
mkkhedawat <writetomansa@gmail.com>
2024-12-14 15:29:11 +0530

    add - day 14 2024

--- utils/plots.ts
+++ utils/plots.ts
@@ -1,28 +1,39 @@
 import * as Plot from "npm:@observablehq/plot";
-import { DOMParser } from "npm:linkedom";
+import { document } from "jsr:@ry/jupyter-helper"
 import { display } from "https://deno.land/x/display@v0.1.1/mod.ts";
 
-const lineGraph = (x, y, labels) => {
-    const document = new DOMParser().parseFromString(
-        `<!DOCTYPE html><html lang="en"></html>`,
-        "text/html",
-    );
+const lineGraph = (x, y, labels, { render = true }) => {
     const data = new Array(x.length).fill(0).map((f, i) => ({
         [labels.x]: x[i],
         [labels.y]: y[i],
     }));
-    
-    return async () => await display(
-        Plot.plot({
-            y: { grid: true },
-            marks: [
-                Plot.line(data, labels),
-            ],
-            document,
-        }),
-    );
+
+    const raw = Plot.plot({
+        y: { grid: true },
+        marks: [
+            Plot.line(data, labels),
+        ],
+        document,
+    });
+
+    if (!render) return raw;
+    return async () => await display(raw);
+}
+
+const scatterGraph = (elements: [], labels, { render = true }) => {
+    const raw = Plot.dot(elements, labels).plot({
+        document,
+    });
+    if (!render) return raw;
+    return async () => await display(raw);
+}
+
+const writeSVG = async (path, svg) => {
+    await Deno.writeTextFile(path, svg);
 }
 
 export default {
-    lineGraph
+    lineGraph,
+    scatterGraph,
+    writeSVG
 }
